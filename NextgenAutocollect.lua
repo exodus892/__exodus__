734f0571adade35f2197b9d4794054f4c8a0765f
exodus892 <diddygameryt@gmail.com>
2026-05-23 14:43:56 +0100

    Update NextgenAutocollect.lua

--- NextgenAutocollect.lua
+++ NextgenAutocollect.lua
@@ -40,7 +40,7 @@ end
 local LocalPlayer = game:GetService("Players").LocalPlayer
 
 local function NextTicketVoucher()
-    return 86400 - (os.time() - game.ReplicatedStorage.Events.RetrievePlayerStats:InvokeServer().SystemTimes["RedeemedTicket Voucher"])
+    return 79200 - (os.time() - game.ReplicatedStorage.Events.RetrievePlayerStats:InvokeServer().SystemTimes["RedeemedTicket Voucher"])
 end
 
 game:GetService("RunService").RenderStepped:Connect(function()
